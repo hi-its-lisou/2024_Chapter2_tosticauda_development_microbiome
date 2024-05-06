b26407d53f864f256f3604a789397391646b3e30
EisenRa <raph.eisenhofer@gmail.com>
2024-05-06 09:02:21 +0200

    add test commit

--- code/Figure2.R
+++ code/Figure2.R
@@ -14,6 +14,8 @@ library(forcats)
 library(ggforce)
 library(vegan)
 
+#test
+
 #load phyloseq object
 ps <- qza_to_phyloseq(
   features = "input_files/merged_table.qza",
@@ -23,6 +25,53 @@ ps <- qza_to_phyloseq(
 ps@sam_data$sampleid = rownames(ps@sam_data)
 ps
 
+ps_acinetobacter <- subset_taxa(ps, Genus=="Acinetobacter")
+df_temp <- as.data.frame(ps_acinetobacter@tax_table) 
+df_temp$asvs <- row.names(ps_acinetobacter@tax_table)
+ps_acinetobacter@tax_table <- tax_table(as.matrix(df_temp))
+supp_adults2023 <- subset_samples(ps_acinetobacter, sample_type %in% c("Adults", "Negative_control"))
+supp_adults2023
+
+top20ASV = names(sort(taxa_sums(supp_adults2023), TRUE)[1:20])
+taxtab20 = cbind(tax_table(supp_adults2023), ASV_20 = NA)
+taxtab20[top20ASV, "ASV_20"] <- as(tax_table(supp_adults2023)
+                                   [top20ASV, "asvs"], "character")
+
+tax_table(supp_adults2023) <- tax_table(taxtab20)
+ps_ASV_ra <- transform_sample_counts(supp_adults2023, function(x) 100 * x/sum(x))
+df_ASV <- psmelt(ps_ASV_ra)
+df_ASV <- arrange(df_ASV, sample_type)
+df_ASV$ASV_20[is.na(df_ASV$ASV_20)] <- c("Other")
+
+
+Acinetobacter_plot_supp <- df_ASV %>%
+  filter(Abundance > 0) %>%
+  ggplot(aes(x = sample_type, y = Abundance, fill = ASV_20)) +
+geom_bar(stat = "identity") +
+facet_nested(~ sample_type + Env_exposure + Sample, scales = "free", space = "free") +
+labs(x = "sample_type", y = "Relative abundance") +
+theme( 
+axis.text.y = element_text(size=16, face = 'bold'),
+axis.title.y = element_text(size=16, face = 'bold'),
+axis.ticks.y = element_line(linewidth = 1),
+axis.ticks.x = element_blank(),
+axis.text.x = element_blank(),
+axis.title.x = element_blank(),
+legend.title = element_blank(),
+legend.text = element_text(size = 18),
+legend.position = "bottom",
+strip.background = element_blank(),
+strip.text = element_textbox_simple(
+padding = margin(5, 0, 5, 0),
+margin = margin(5, 5, 5, 5),
+size = 10,
+face = "bold",
+halign = 0.5,
+fill = "white",
+box.color = "grey",
+linewidth = 1.5,
+linetype = "solid",),
+panel.background = element_blank())
 
 #Load costum colour palette 
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
