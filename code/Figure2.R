a65675ee37726c7e68156b491ad791a804c1e2a0
hi-its-lisou <elisabeth.williamson@adelaide.edu.au>
2024-05-15 11:38:21 +0930

    Updated colour palette code and titles

--- code/Figure2.R
+++ code/Figure2.R
@@ -1,4 +1,4 @@
-#load required libraries
+# Load the required libraries ####
 library(phyloseq)
 library(tidyverse)
 library(qiime2R)
@@ -14,7 +14,7 @@ library(forcats)
 library(ggforce)
 library(vegan)
 
-#load phyloseq object
+# Load phyloseq object ####
 ps <- qza_to_phyloseq(
   features = "input_files/merged_table.qza",
   taxonomy = "input_files/merged_taxonomy.qza",
@@ -23,106 +23,42 @@ ps <- qza_to_phyloseq(
 ps@sam_data$sampleid = rownames(ps@sam_data)
 ps
 
-#Load costum colour palette 
-getPalette = colorRampPalette(brewer.pal(9, "Set1"))
-GenusList = unique(tax_table(ps)[,"Genus"])
-GenusPalette = getPalette(length(GenusList))
-names(GenusPalette) = GenusList
-GenusPalette["Other"] <- "grey"
-new_color <- "#3D550C"
-taxon_to_change <- "Erwinia"
-GenusPalette[taxon_to_change] <- new_color
-new_color2 <- "#81B622"
-taxon_to_change2 <- "Arsenophonus"
-GenusPalette[taxon_to_change2] <- new_color2
-new_color3 <- "#ECF87F"
-taxon_to_change3 <- "Lactobacillus"
-GenusPalette[taxon_to_change3] <- new_color3
-new_color4 <- "#59981A"
-taxon_to_change4 <- "Dolosigranulum"
-GenusPalette[taxon_to_change4] <- new_color4
-new_color5 <- "#F51720"
-taxon_to_change5 <- "Sodalis"
-GenusPalette[taxon_to_change5] <- new_color5
-new_color6 <- "#FA26A0"
-taxon_to_change6 <- "Acinetobacter"
-GenusPalette[taxon_to_change6] <- new_color6
-new_color7 <- "#F8D210"
-taxon_to_change7 <- "Tyzerella"
-GenusPalette[taxon_to_change7] <- new_color7
-new_color8 <- "#2FF3E0"
-taxon_to_change8 <- "Sphingomonas"
-GenusPalette[taxon_to_change8] <- new_color8
-new_color9 <- "#B1D4E0"
-taxon_to_change9 <- "Skermanella"
-GenusPalette[taxon_to_change9] <- new_color9
-new_color10 <- "#2E8BC0"
-taxon_to_change10 <- "Gilliamella"
-GenusPalette[taxon_to_change10] <- new_color10
-new_color11 <- "#0C2D48"
-taxon_to_change11 <- "Aquabacterium"
-GenusPalette[taxon_to_change11] <- new_color11
-new_color12 <- "#145DA0"
-taxon_to_change12 <- "Thauera"
-GenusPalette[taxon_to_change12] <- new_color12
-new_color14 <- "#FFD4DB"
-taxon_to_change14 <- "Massilia"
-GenusPalette[taxon_to_change14] <- new_color14
-new_color15 <- "#D3B5E5"
-taxon_to_change15 <- "Modestobacter"
-GenusPalette[taxon_to_change15] <- new_color15
-new_color17 <- "#D8A7B1"
-taxon_to_change17 <- "Asaia"
-GenusPalette[taxon_to_change17] <- new_color17
-new_color20 <- "#EF7C8E"
-taxon_to_change20 <- "Carnimonas"
-GenusPalette[taxon_to_change20] <- new_color20
-new_color21 <- "#0023F5"
-taxon_to_change21 <- "Bacillus"
-GenusPalette[taxon_to_change21] <- new_color21
-new_color22 <- "#A020F0"
-taxon_to_change22 <- "Enterococcus"
-GenusPalette[taxon_to_change22] <- new_color22
-new_colorX <- "#000000"
-taxon_to_changeX <- "Mitochondria"
-GenusPalette[taxon_to_changeX] <- new_colorX
-new_colorY <- "#008000"
-taxon_to_changeY <- "Chloroplast"
-GenusPalette[taxon_to_changeY] <- new_colorY
+# Load custom colour palette ####
+colours_df <- file.path(
+  "D:/Research/PhD/Manuscripts/Chapter 2/Git/2024_Chapter2_tosticauda_development_microbiome/input_files/",
+  "colour_list.csv"
+) %>% read_csv
 
-#rarefy at 1500
+my_palette <- colours_df$colours
+names(my_palette) <- colours_df$genera
+my_palette
+
+# Rarefy ####
 ps_rar <- rarefy_even_depth(ps, sample.size = 1500, rngseed = 1337)
-ps_rar
 
-#import contaminant asvs
+# Remove contaminants, chloroplasts, and mitochondria ####
 contam <- read_delim("input_files/chloro_mito_decontam_asvs.txt", 
                      delim = "\n", 
                      col_names = "asv")
-
 chloro_mito_decontam_asvs <- contam$asv
-
-#vector for chloroplast, mitochondrial, and contaminant asvs
-chloro_mito_decontam_asvs
-
-#remove off-target reads
 all_asvs <- taxa_names(ps_rar)
 asvs_to_keep <- all_asvs[!(all_asvs %in% chloro_mito_decontam_asvs)]
 ps_rar <- prune_taxa(asvs_to_keep, ps_rar)
 ps_rar
 
-#filter out 0 abundance reads
+# Filter out 0 abundance reads ####
 zero_abundance_samples <- sample_sums(ps_rar) == 0
 filtered_physeq <- subset_samples(ps_rar, !zero_abundance_samples)
 filtered_physeq
 
-#number of reads per sample
+# Determine the number of reads per sample ####
 sort(sample_sums(filtered_physeq))
 
-# Subset the data for samples_types 
+# Subset the data for samples_types ####
 food_through_time <- subset_samples(filtered_physeq, sample_type %in% c("Food") 
                                     & Nest_id %in% c("5", "13", "14", "27")
                                     & Cell_ID %in% c("J", "I", "H", "G", "F", "E", "D", "C", "B", "A"))
-#obtain top 20 genera
+# Obtain top 20 genera ####
 ps_Genus <- tax_glom(food_through_time, taxrank = "Genus", NArm = FALSE)
 top20Genus = names(sort(taxa_sums(ps_Genus), TRUE)[1:20])
 taxtab20 = cbind(tax_table(ps_Genus), Genus_20 = NA)
@@ -133,21 +69,26 @@ ps_Genus_ra <- transform_sample_counts(ps_Genus, function(x) 100 * x/sum(x))
 df_Genus <- psmelt(ps_Genus_ra)
 df_Genus <- arrange(df_Genus, sample_type)
 df_Genus$Genus_20[is.na(df_Genus$Genus_20)] <- c("Other")
+
+# % of reads that make up the top 20 genera
 mean(
   sample_sums(
     prune_taxa(top20Genus, ps_Genus_ra)
   )
 )
 
-# custom order for Cell_ID from youngest to oldest
+
+
+# Plot the relative abundance ####
+# Custom order for Cell_ID from youngest to oldest
 custom_Cell_ID_order <- c("J", "I", "H", "G", "F", "E", "D", "C", "B", "A")
 df_Genus$Cell_ID <- factor(df_Genus$Cell_ID, levels = custom_Cell_ID_order)
-                                       
-fig3 <- df_Genus %>%
+
+(fig3 <- df_Genus %>%
   mutate(Genus_20 = reorder(Genus_20, -Abundance)) %>%
   ggplot(aes(x = sample_type, y = Abundance, fill = Genus_20)) +
   geom_bar(width = 1, stat = "identity") +
-  scale_fill_manual(values = GenusPalette) +
+  scale_fill_manual(values = my_palette) +
   facet_nested(~ Nest_id + Cell_ID + sample_type, scales = "free", space = "free") +
   labs(x = "sample_type", y = "Relative abundance") +
   theme( 
@@ -172,88 +113,18 @@ fig3 <- df_Genus %>%
       linetype = "solid",
       family = "Calibri"),
     panel.background = element_blank()
-  )
+  ))
 
-fig3
-
-#alpha diversity of pollen provisions as they are consumed
+# Alpha diversity of pollen provisions as they are consumed ####
 alpha_diversity <- alpha(food_through_time, index = "Shannon")
 metadata <- meta(food_through_time)
 metadata$name <- rownames(metadata)
 alpha_diversity$name <- rownames(alpha_diversity)
 alpha_diversity_metadata <- merge(alpha_diversity, metadata, by = "name")
 
+# Box plot for alpha diversity
 alpha_diversity_metadata %>%
   ggplot(aes(x = Cell_ID, y = diversity_shannon, colour = Cell_ID)) +
   geom_boxplot() +
   geom_point(size = 3)
 
-#### Acinetobacter plot - Supplementary Figure 2 ###
-# Phyloseq object with only Acinetobacter ASVs
-ps_acinetobacter <- subset_taxa(ps, Genus=="Acinetobacter")
-df_temp <- as.data.frame(ps_acinetobacter@tax_table) 
-df_temp$asvs <- row.names(ps_acinetobacter@tax_table)
-ps_acinetobacter@tax_table <- tax_table(as.matrix(df_temp))
-
-# Subset samples
-supp_adults2023 <- subset_samples(ps_acinetobacter, sample_type %in% c("Adults", "Negative_control"))
-supp_adults2023
-
-sample_sums(supp_adults2023)
-
-# Sort top 20 ASVs
-top20ASV = names(sort(taxa_sums(supp_adults2023), TRUE)[1:20])
-taxtab20 = cbind(tax_table(supp_adults2023), ASV_20 = NA)
-taxtab20[top20ASV, "ASV_20"] <- as(tax_table(supp_adults2023)
-                                   [top20ASV, "asvs"], "character")
-
-tax_table(supp_adults2023) <- tax_table(taxtab20)
-ps_ASV_ra <- transform_sample_counts(supp_adults2023, function(x) 100 * x/sum(x))
-df_ASV <- psmelt(ps_ASV_ra)
-df_ASV <- arrange(df_ASV, sample_type)
-df_ASV$ASV_20[is.na(df_ASV$ASV_20)] <- c("Other")
-
-# Colour palette to distinguish true Acinetobacter from contam
-getPalette = colorRampPalette(brewer.pal(9, "Set1"))
-ASVList = unique(tax_table(ps_acinetobacter)[,"asvs"])
-ASVPalette = getPalette(length(ASVList))
-names(ASVPalette) = ASVList
-
-new_color_Acinetobacter <- "#FA26A0"
-Acinetobacter_to_change <- "c624f2e4228eea7296b2a77e2d4b7e50"
-ASVPalette[Acinetobacter_to_change] <- new_color_Acinetobacter
-
-#Plot relative abundance of Acinetobacter ASVs
-Acinetobacter_plot_supp2 <- df_ASV %>%
-  filter(Abundance > 0) %>%
-  ggplot(aes(x = sample_type, y = Abundance, fill = ASV_20)) +
-  geom_bar(stat = "identity") +
-  scale_fill_manual(values = ASVPalette) +
-  facet_nested(~ sample_type + Env_exposure + Sample, scales = "free", space = "free") +
-  labs(x = "sample_type", y = "Relative abundance") +
-  theme( 
-    axis.text.y = element_text(size=16, face = 'bold'),
-    axis.title.y = element_text(size=16, face = 'bold'),
-    axis.ticks.y = element_line(linewidth = 1),
-    axis.ticks.x = element_blank(),
-    axis.text.x = element_blank(),
-    axis.title.x = element_blank(),
-    legend.title = element_blank(),
-    legend.text = element_text(size = 18),
-    legend.position = "bottom",
-    strip.background = element_blank(),
-    strip.text = element_textbox_simple(
-      padding = margin(5, 0, 5, 0),
-      margin = margin(5, 5, 5, 5),
-      size = 16,
-      face = "bold",
-      halign = 0.5,
-      fill = "white",
-      box.color = "grey",
-      linewidth = 1.5,
-      linetype = "solid",),
-    panel.background = element_blank())
-
-
-Acinetobacter_plot_supp2
-ggsave("FigureS2.png", height=15, width=25)
