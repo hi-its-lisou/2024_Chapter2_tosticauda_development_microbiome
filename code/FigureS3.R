632f2614cf42ad62903a339bf0ab640fc49c9993
hi-its-lisou <114640546+hi-its-lisou@users.noreply.github.com>
2024-08-20 16:55:26 +0930

    Updated

--- code/FigureS3.R
+++ code/FigureS3.R
@@ -33,19 +33,13 @@ supp_adults2023
 # Determine the number of reads per sample ####
 sample_sums(supp_adults2023)
 
-# Sort top 20 ASVs
-top20ASV = names(sort(taxa_sums(supp_adults2023), TRUE)[1:20])
-taxtab20 = cbind(tax_table(supp_adults2023), ASV_20 = NA)
-taxtab20[top20ASV, "ASV_20"] <- as(tax_table(supp_adults2023)
-                                   [top20ASV, "asvs"], "character")
-
-tax_table(supp_adults2023) <- tax_table(taxtab20)
+# No need to filter to top 20 ASVs
+# Simply transform and melt the entire dataset
 ps_ASV_ra <- transform_sample_counts(supp_adults2023, function(x) 100 * x/sum(x))
 df_ASV <- psmelt(ps_ASV_ra)
 df_ASV <- arrange(df_ASV, sample_type)
-df_ASV$ASV_20[is.na(df_ASV$ASV_20)] <- c("Other")
 
-# Colour palette to distinguish true Acinetobacter from contam
+# Colour palette to distinguish true Acinetobacter from contaminants
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 ASVList = unique(tax_table(ps_acinetobacter)[,"asvs"])
 ASVPalette = getPalette(length(ASVList))
@@ -54,38 +48,36 @@ new_color_Acinetobacter <- "#FA26A0"
 Acinetobacter_to_change <- "c624f2e4228eea7296b2a77e2d4b7e50"
 ASVPalette[Acinetobacter_to_change] <- new_color_Acinetobacter
 
-#Plot relative abundance of Acinetobacter ASVs
-Acinetobacter_plot_supp2 <- df_ASV %>%
-  filter(Abundance > 0) %>%
-  ggplot(aes(x = sample_type, y = Abundance, fill = ASV_20)) +
-  geom_bar(stat = "identity") +
-  scale_fill_manual(values = ASVPalette) +
-  facet_nested(~ sample_type + Env_exposure + Sample, scales = "free", space = "free") +
-  labs(x = "sample_type", y = "Relative abundance") +
-  theme( 
-    axis.text.y = element_text(size=16, face = 'bold'),
-    axis.title.y = element_text(size=16, face = 'bold'),
-    axis.ticks.y = element_line(linewidth = 1),
-    axis.ticks.x = element_blank(),
-    axis.text.x = element_blank(),
-    axis.title.x = element_blank(),
-    legend.title = element_blank(),
-    legend.text = element_text(size = 18),
-    legend.position = "bottom",
-    strip.background = element_blank(),
-    strip.text = element_textbox_simple(
-      padding = margin(5, 0, 5, 0),
-      margin = margin(5, 5, 5, 5),
-      size = 16,
-      face = "bold",
-      halign = 0.5,
-      fill = "white",
-      box.color = "grey",
-      linewidth = 1.5,
-      linetype = "solid",),
-    panel.background = element_blank())
-
-Acinetobacter_plot_supp2
+# Plot relative abundance of all Acinetobacter ASVs
+(Acinetobacter_plot_supp2 <- df_ASV %>%
+    filter(Abundance > 0) %>%
+    ggplot(aes(x = sample_type, y = Abundance, fill = asvs)) +
+    geom_bar(stat = "identity") +
+    scale_fill_manual(values = ASVPalette) +
+    facet_nested(~ sample_type + Env_exposure + Sample, scales = "free", space = "free") +
+    labs(x = "sample_type", y = "Relative abundance") +
+    theme( 
+      axis.text.y = element_text(size=16, face = 'bold'),
+      axis.title.y = element_text(size=16, face = 'bold'),
+      axis.ticks.y = element_line(linewidth = 1),
+      axis.ticks.x = element_blank(),
+      axis.text.x = element_blank(),
+      axis.title.x = element_blank(),
+      legend.title = element_blank(),
+      legend.text = element_text(size = 14),
+      legend.position = "bottom",
+      strip.background = element_blank(),
+      strip.text = element_textbox_simple(
+        padding = margin(5, 0, 5, 0),
+        margin = margin(5, 5, 5, 5),
+        size = 16,
+        face = "bold",
+        halign = 0.5,
+        fill = "white",
+        box.color = "grey",
+        linewidth = 1.5,
+        linetype = "solid",),
+      panel.background = element_blank()))
 
 # Save plot
-ggsave("FigureS2.png", height=15, width=25)
+ggsave("figures/FigureS2.png", height=10, width=20)
